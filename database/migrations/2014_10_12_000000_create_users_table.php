49049948f18ebcdce1119bb293fce6e04e7f6ab4
Ngugi Kiarie <ngugithe4th@gmail.com>
2021-02-02 17:06:26 +0300

    Set defaults in db migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -19,16 +21,32 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('last_login');
-            $table->string('online');
+            $table->string('last_login')->nullable();
+            $table->string('online')->default(0);
             $table->integer('active')->default(1);
             $table->integer('deleted_by')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
-    }
 
+        DB::table('users')->insert(
+            array(
+                'id' => 1,
+                'name' => 'Admin Default',
+                'email' => 'admin@pamas.ichaelinc.co.ke',
+                'password' => Hash::make('admin@pamas'),
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'id' => 2,
+                'name' => 'User Default',
+                'email' => 'user@pamas.ichaelinc.co.ke',
+                'password' => Hash::make('user@pamas'),
+            )
+        );
+    }
     /**
      * Reverse the migrations.
      *
--- database/migrations/2021_01_27_080124_create_permission_tables.php
+++ database/migrations/2021_01_27_080124_create_permission_tables.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreatePermissionTables extends Migration
 {
@@ -26,6 +27,69 @@ class CreatePermissionTables extends Migration
             $table->string('guard_name');
             $table->timestamps();
         });
+        DB::table('permissions')->insert(
+            array(
+                'id' => 1,
+                'name' => 'All',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 2,
+                'name' => 'Add Super-Admin',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 3,
+                'name' => 'Add Admin',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 4,
+                'name' => 'Add User',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 5,
+                'name' => 'Process Payroll',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 6,
+                'name' => 'Edit Settings',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('permissions')->insert(
+            array(
+                'id' => 7,
+                'name' => 'Delete Staff',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
 
         Schema::create($tableNames['roles'], function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -33,6 +97,44 @@ class CreatePermissionTables extends Migration
             $table->string('guard_name');
             $table->timestamps();
         });
+        // Defaults
+        DB::table('roles')->insert(
+            array(
+                'id' => 1,
+                'name' => 'Super-Admin',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('roles')->insert(
+            array(
+                'id' => 2,
+                'name' => 'Admin',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('roles')->insert(
+            array(
+                'id' => 3,
+                'name' => 'Accountant',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+        DB::table('roles')->insert(
+            array(
+                'id' => 4,
+                'name' => 'User',
+                'guard_name' => 'Web',
+                'created_at' => Date('Y-m-d H:i:s'),
+                'updated_at' => Date('Y-m-d H:i:s'),
+            )
+        );
+
 
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
             $table->unsignedBigInteger('permission_id');
@@ -46,8 +148,10 @@ class CreatePermissionTables extends Migration
                 ->on($tableNames['permissions'])
                 ->onDelete('cascade');
 
-            $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+            $table->primary(
+                ['permission_id', $columnNames['model_morph_key'], 'model_type'],
+                'model_has_permissions_permission_model_type_primary'
+            );
         });
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
@@ -62,10 +166,19 @@ class CreatePermissionTables extends Migration
                 ->on($tableNames['roles'])
                 ->onDelete('cascade');
 
-            $table->primary(['role_id', $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+            $table->primary(
+                ['role_id', $columnNames['model_morph_key'], 'model_type'],
+                'model_has_roles_role_model_type_primary'
+            );
         });
 
+        $mhr = [
+            ['role_id' => 1, 'model_type' => 'App\Models\User', 'model_id' => 1],
+            ['role_id' => 2, 'model_type' => 'App\Models\User', 'model_id' => 13],
+            ['role_id' => 3, 'model_type' => 'App\Models\User', 'model_id' => 16],
+            ['role_id' => 4, 'model_type' => 'App\Models\User', 'model_id' => 17],
+        ];
+        DB::table('model_has_roles')->insert($mhr);
         Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
             $table->unsignedBigInteger('permission_id');
             $table->unsignedBigInteger('role_id');
@@ -83,6 +196,50 @@ class CreatePermissionTables extends Migration
             $table->primary(['permission_id', 'role_id'], 'role_has_permissions_permission_id_role_id_primary');
         });
 
+        $rhp = [
+            [
+                'permission_id' => 1,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 2,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 3,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 3,
+                'role_id' => 2,
+            ], [
+                'permission_id' => 4,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 4,
+                'role_id' => 2,
+            ], [
+                'permission_id' => 5,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 5,
+                'role_id' => 2,
+            ], [
+                'permission_id' => 5,
+                'role_id' => 3,
+            ], [
+                'permission_id' => 6,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 6,
+                'role_id' => 2,
+            ], [
+                'permission_id' => 7,
+                'role_id' => 1,
+            ], [
+                'permission_id' => 7,
+                'role_id' => 2,
+            ],
+        ];
+
+        DB::table('role_has_permissions')->insert($rhp);
         app('cache')
             ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
             ->forget(config('permission.cache.key'));
